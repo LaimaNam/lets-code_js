bf66bc987953edeac576a4b0f730dc79c2db54c3
LaimaNam <laima.namajunaite@gmail.com>
2021-06-16 12:08:54 +0300

    update advert function added

--- FETCH_api_+_REST_API/1_frontend/index.js
+++ FETCH_api_+_REST_API/1_frontend/index.js
@@ -2,36 +2,52 @@
 const ALL_ADVERTS = 'http://localhost:5000/adverts/';
 
 //Variables
+// DOM elements
 const advertsListElement = document.querySelector('#adverts-list');
 const formElement = document.querySelector('form');
 const successErrorMSG = document.querySelector('#success-error-msg');
+
+// Logic
+// --- id to update
+let id;
+
 //Functions
 function getAndShowAllAdverts() {
   return fetch(ALL_ADVERTS)
     .then((response) => response.json())
     .then((data) => {
+      // let adverts = data.reverse();
       let adverts = data;
       let output = '';
 
       for (let advert of adverts) {
         output += `
-          <div>
+          <div id="${advert.id}">
           <h3>${advert.name}</h3>
           <p>Price: ${advert.price}</p>
-          <p>Contact phone: ${advert.phone}</p>
-
+          <p>Phone: ${advert.phone}</p>
+          <button class="update-btn" data-id="${advert.id}">UPDATE</button>
+          <button class="delete-btn" data-id="${advert.id}">DELETE</button>
           </div>
           `;
       }
+
       advertsListElement.innerHTML = output;
+
+      const updateBtns = document.querySelectorAll('.update-btn');
+
+      updateBtns.forEach((btn) => btn.addEventListener('click', updateForm));
     });
 }
 
+// --- POST data
 function sendAdvert(e) {
   e.preventDefault();
+
   let name = e.target.name.value;
   let price = e.target.price.value;
   let phone = e.target.phone.value;
+
   if (!name || !price || !phone) {
     return (successErrorMSG.innerText = 'Please fill all the fields!');
   } else {
@@ -47,10 +63,64 @@ function sendAdvert(e) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      successErrorMSG.innerText = data.message;
       getAndShowAllAdverts();
+      formElement.reset();
     });
 }
+
+//PUT (update data)
+function updateForm(e) {
+  const advertElements = e.target.parentElement.children;
+  id = +e.target.dataset.id;
+
+  const [name, price, phone] = advertElements;
+
+  // --form inputs
+  const nameInput = document.querySelector('#name');
+  const priceInput = document.querySelector('#price');
+  const phoneInput = document.querySelector('#phone');
+
+  nameInput.value = name.innerText;
+  priceInput.value = price.innerText.slice(7);
+  phoneInput.value = phone.innerText.slice(7);
+
+  formElement.removeEventListener('submit', sendAdvert);
+  formElement.addEventListener('submit', updateAdvert);
+}
+
+function updateAdvert(e) {
+  e.preventDefault();
+
+  let name = e.target.name.value;
+  let price = e.target.price.value;
+  let phone = e.target.phone.value;
+
+  if (!name || !price || !phone) {
+    return (successErrorMSG.innerText = 'Please fill all the fields!');
+  } else {
+    successErrorMSG.innerText = '';
+  }
+
+  return fetch(ALL_ADVERTS + id, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, name, price, phone }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      successErrorMSG.innerText = data.message;
+      getAndShowAllAdverts();
+      formElement.reset();
+
+      formElement.removeEventListener('submit', updateAdvert);
+      formElement.addEventListener('submit', sendAdvert);
+    });
+}
+
 //Events
 document.addEventListener('DOMContentLoaded', getAndShowAllAdverts);
 formElement.addEventListener('submit', sendAdvert);
--- FETCH_api_+_REST_API/2_backend/index.js
+++ FETCH_api_+_REST_API/2_backend/index.js
@@ -7,30 +7,50 @@ app.use(express.json());
 
 // ADVERTS DB - simulation
 let adverts = [
-  { id: 1, name: 'PS5', price: 800, phone: '+37067438970' },
-  { id: 2, name: 'X-box', price: 900, phone: '+37067438555' },
-  { id: 3, name: 'X-box', price: 1900, phone: '+37067438555' },
+  { id: 1, name: 'PS5', price: '800', phone: '+37067438970' },
+  { id: 2, name: 'X-box', price: '900', phone: '+37067438555' },
+  { id: 3, name: 'X-box', price: '1900', phone: '+37067438555' },
 ];
 
 // ROUTES (REST API)
 // --GET all adverts
-app.get('/adverts', (req, res) => {
+app.get('/adverts/', (req, res) => {
   res.send(adverts);
 });
 // --GET single advert based on id
-app.get('/adverts/:id', (req, res) => {
+app.get('/adverts/:id/', (req, res) => {
   res.send(adverts.filter((item) => item.id === +req.params.id));
 });
 
-// POST
-app.post('/adverts', (req, res) => {
+// POST ----------------------------------------
+app.post('/adverts/', (req, res) => {
   let id = adverts[adverts.length - 1].id + 1;
 
   adverts.push({ id, ...req.body });
-  res.send(adverts);
+
+  res.send({ data: adverts, message: 'New advert added!' });
 });
 
-// ... starting server
+//PUT-------------------------------------------
+app.put('/adverts/:id/', (req, res) => {
+  let updatedAdvert = req.body;
+  let advertID = +req.params.id;
+
+  let updatedAdverts = adverts.map((advert) => {
+    if (advert.id === advertID) {
+      return { ...updatedAdvert };
+    } else {
+      return advert;
+    }
+  });
+  console.log(updatedAdvert);
+
+  adverts.length = 0;
+  adverts.push(...updatedAdverts);
+  res.send({ data: updatedAdverts, message: 'Advert updated!' });
+});
+
+// ... starting server-------------------------------------
 app.listen(5000, () => console.log('Server is running on port: 5000'));
 
 //  ---------------- TEORIJA -------------------
@@ -44,7 +64,10 @@ app.listen(5000, () => console.log('Server is running on port: 5000'));
 //--- creating REST API routes
 //GET:
 //     ----/adverts - get all adverts
-//     ----/adverts/:id - advert based on id
+//     ----/adverts/:id - get advert based on id
 // POST:
 //     ---ideti kazka naujo
 //     ---/adverts - add to all adverts
+
+//PUT:
+//   --- /adverts/:id - atnaujinti vieną item'ą based on id
